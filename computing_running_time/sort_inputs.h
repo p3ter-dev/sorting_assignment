f66b437277ce96959f36010061e7479fe8f65f71
peter <peterkinfe548@gmail.com>
2025-06-11 18:32:01 +0300

    input lists header

---
+++ computing_running_time/sort_inputs.h
@@ -0,0 +1,12 @@
+#ifndef SORT_INPUTS_H
+#define SORT_INPUTS_H
+
+#include <vector>
+using namespace std;
+
+vector<int> generateRandomList(int size);
+vector<int> generateAscendingList(int size);
+vector<int> generateDescendingList(int size);
+vector<int> generateFewItemsList();
+
+#endif
